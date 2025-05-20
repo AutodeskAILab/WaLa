e8e2d7a018ec1a5035b7b3567ac56c8a60847958
giuliaa <alessandro.giuliano@autodesk.com>
2025-05-20 20:21:27 +0000

    Added run baseline file

---
+++ run_baseline_testing.py
@@ -0,0 +1,135 @@
+from pathlib import Path
+import open3d as o3d
+import os
+
+from pytorch_lightning import seed_everything
+
+from src.dataset_utils import (
+    get_singleview_data,
+    get_multiview_data,
+    get_voxel_data_json,
+    get_image_transform_latent_model,
+    get_pointcloud_data,
+    get_mv_dm_data,
+    get_sv_dm_data,
+    get_sketch_data
+)
+from src.model_utils import Model
+from src.mvdream_utils import load_mvdream_model
+import argparse
+from PIL import Image
+
+from comet_ml import start
+from comet_ml.integration.pytorch import log_model
+
+experiment = start(
+  api_key="mqrUAXjKBRul24uX6pxR3gRHX*eyJiYXNlVXJsIjoiaHR0cHM6Ly9jb21ldC5kZXYuY2xvdWRvcy5hdXRvZGVzay5jb20ifQ",
+  project_name="wala-time-checks",
+  workspace="alessandro-giuliano"
+)
+
+
+
+def simplify_mesh(obj_path, target_num_faces=1000):
+    mesh = o3d.io.read_triangle_mesh(obj_path)
+    simplified_mesh = mesh.simplify_quadric_decimation(target_num_faces)
+    o3d.io.write_triangle_mesh(obj_path, simplified_mesh)
+
+
+def generate_3d_object(
+    model,
+    data,
+    data_idx,
+    scale,
+    diffusion_rescale_timestep,
+    save_dir="examples",
+    output_format="obj",
+    target_num_faces=None,
+    seed=42,
+    
+):
+    # Set seed
+    seed_everything(seed, workers=True)
+
+    save_dir.mkdir(parents=True, exist_ok=True)
+    model.set_inference_fusion_params(scale, diffusion_rescale_timestep)
+    output_path = model.test_inference(
+        data, data_idx, experiment, save_dir=save_dir, output_format=output_format
+    )
+
+    if output_format == "obj" and target_num_faces:
+        simplify_mesh(output_path, target_num_faces=target_num_faces)
+
+
+
+model_name = 'ADSKAILab/WaLa-SV-1B'
+images_path = Path('examples/single_view/')
+single_image = ['examples/single_view/table.png']
+
+output_dir = 'examples/Test_Gen'
+output_format = 'obj'
+target_num_faces = None
+scale = 1.8
+seed = 42
+diffusion_rescale_timestep = 5
+
+
+  # Define the hyperparameters
+hyperparameters = {
+    "model_name": model_name,
+    "images_path": images_path,
+    "single_image": single_image,
+    "output_dir": output_dir,
+    "output_format": output_format,
+    "target_num_faces": None,
+    "scale": scale,
+    "seed": seed,
+    "diffusion_rescale_timestep": 5
+}
+
+# Log the hyperparameters to Comet
+experiment.log_parameters(hyperparameters)
+
+
+
+### Scanned Objects by Google Research
+google_test_img_path = Path(os.path.expanduser('~/test_images'))  # Expands '~' to the full home directory path
+output_dir = os.path.expanduser('~/Obj_generated')
+
+for idx, image_path in enumerate(google_test_img_path.iterdir()):  # Enumerate to get the index
+        #if idx >= 3:  # Stop after processing the first 3 files
+            #break
+        print(f"Processing image {idx + 1}: {image_path}")
+
+
+
+        timer1 = time.time()
+        data = get_singleview_data(
+            image_file=Path(image_path),
+            image_transform=image_transform,
+            device=model.device,
+            image_over_white=False,
+        )
+        data_idx = 0
+        save_dir = Path(output_dir) 
+
+        model.set_inference_fusion_params(
+            scale, diffusion_rescale_timestep
+        )
+
+        generate_3d_object(
+            model,
+            data,
+            data_idx,
+            scale,
+            diffusion_rescale_timestep,
+            save_dir,
+            output_format,
+            target_num_faces,
+            seed,
+        )
+        delta = time.time() - timer1
+        experiment.log_metric("Default Delta", delta)
+        print('Total Inference time', delta)
+
+experiment.end()
